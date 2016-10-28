c4e95ae86ad012106901369030a912bcdb05778d
arsenykonohov <arsenykonohov@gmail.com>
2016-10-29 01:27:24 +0300

    6.6. Styles and assets | ExtractTextPlugin - #2 preparing for creation dynamic module (main)

--- _source/page/index.js
+++ _source/page/index.js
@@ -1,26 +1,66 @@
 "use strict";
 // ----------------------------------------------------
-import "./style.page.scss";
 import "./flag/style.flag.css";
+
+import "./style.page.scss";
+import "./style.content.scss";
+import "./style.badge.scss";
+import "./style.table.scss";
+import "./style.form.scss";
 
 import footer from "./footer";
 import header from "./header";
-import main   from "./main";
 import Menu   from "./nav";
+import Main   from "./main";
+
+// ----------------------------------------------------
+let main = new Main();
+let mainGenerator = main.generateContent();
+mainGenerator("home");
 
 // ----------------------------------------------------
 let navInputData = ["home", "product", "login"];
 let navGenrator = new Menu(navInputData, "js_nav", "nav");
 navGenrator.createMenu();
+navGenrator.createListeners(mainGenerator);
 
-// ----------------------------------------------------
-function generateMainPage(route) {
-    main.container.innerHTML = main.tmp[route](main.options[route]);
-};
-navGenrator.createListeners(generateMainPage);
-
-
-
+//// =======================================================================================================
+//let handler = function (e) {
+//    let text = e.target.innerHTML.replace(/\s+/g, "");
+//    require.ensure([], function (require) {
+//        //let routes = require("./routes/" + text + "/index.js");
+//        // ------------------------------------------------------------------------ 4.5 start
+//        // async call (easy way to do it):
+//        //require("bundle!./routes/" + text + "/index.js")((routes) => {
+//        //    let result = routes(text);
+//        //    mainContainer.innerHTML = result;
+//        //});
+//        // ---------------------------- how to catch exception with incorrect path to module:
+//        let handler;
+//        try {
+//            //handler = require("bundle!./routes/" + text + "/index.js"); // #2
+//            
+//            let context = require.context("bundle!./routes/", true, /^\.\//);
+//            handler = context("./" + text + "/index.js");
+//        } catch (e) {
+//            console.log("-------------------------------------------------");
+//            console.log(e);
+//            console.log("-------------------------------------------------");
+//        }
+//        // async call:
+//        if (handler) {
+//            handler((routes) => {
+//                let result = routes(text);
+//                mainContainer.innerHTML = result;
+//            });
+//        }
+//        // ------------------------------------------------------------------------ 4.5 end
+//        //let routes = require("bundle!./routes/" + text + "/index.js");
+//        //let result = routes(text);
+//        //mainContainer.innerHTML = result;
+//    });
+//};
+//// =======================================================================================================
 
 
 // ----------------------------------------------------
--- _source/page/main/index.js
+++ _source/page/main/index.js
@@ -1,40 +1,45 @@
 "use strict";
 // ----------------------------------------------------
 import "./style.main.css";
-import "./style.content.scss";
-import "./style.badge.scss";
-import "./style.table.scss";
-import "./style.form.scss";
-
 // ----------------------------------------------------
 import homeTmp    from "./main-home/tmp.main-home.pug"
 import productTmp from "./main-product/tmp.main-product.pug"
 import loginTmp   from "./main-login/tmp.main-login.pug"
-let container = document.getElementById("js_main");
 // ----------------------------------------------------
 let optionsHome = {
     title: "home-page"
 };
 let optionsProduct = {
     title: "product-page",
-    list: []
 };
 let optionsLogin = {
     title: "login-page"
 };
-let options = {
-    home: optionsHome,
-    product: optionsProduct,
-    login: optionsLogin
-}
+
 // ----------------------------------------------------
-export default {
-    options: options,
-    name: "main",
-    container: container,
-    tmp: {
-        home: homeTmp,
-        product: productTmp,
-        login: loginTmp
-    }
+export default class Main {
+    constructor() {
+        this.name = "main";
+        this.container = document.getElementById("js_main");
+        this.options = {
+            home: optionsHome,
+            product: optionsProduct,
+            login: optionsLogin
+        };
+        this.tmp = {
+            home: homeTmp,
+            product: productTmp,
+            login: loginTmp
+        };
+    };
+    
+    generateContent() {
+        let main = {
+            name: this.name,
+            container: this.container,
+            options: this.options,
+            tmp: this.tmp
+        }
+        return ((route) => main.container.innerHTML = main.tmp[route](main.options[route]));
+    };
 }
